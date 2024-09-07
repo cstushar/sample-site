1a5c6080d986f14882012871efd7aa990b40468f
Tushar Gupta <61007323+cstushar@users.noreply.github.com>
2024-09-07 21:03:16 +0530

    Update stop_apache.sh

--- scripts/stop_apache.sh
+++ scripts/stop_apache.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# stoping apache
-rm -rf /var/www/html/*
-service httpd stop
+if systemctl is-active --quiet httpd; then
+  echo "Stopping Apache..."
+  systemctl stop httpd
+else
+  echo "Apache is not running."
+fi
